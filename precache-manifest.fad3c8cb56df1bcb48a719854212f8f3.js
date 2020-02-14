2c8dfa2aa92edd3efecab183e5e2edab1bf51ce6
Misa Jokisalo <misa.jokisalo@joinex.com>
2020-02-14 18:23:06 +0200

    Updates

---
+++ precache-manifest.fad3c8cb56df1bcb48a719854212f8f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff7308fe62018f24bafc133d05e9d82",
+    "url": "/ystavanpaiva/index.html"
+  },
+  {
+    "revision": "4ddc8494936ecbd3ca05",
+    "url": "/ystavanpaiva/static/js/2.9fe71024.chunk.js"
+  },
+  {
+    "revision": "4029acd0d70119d8996d",
+    "url": "/ystavanpaiva/static/js/main.f80bd83e.chunk.js"
+  },
+  {
+    "revision": "4bab3f2d1975aa0593a2",
+    "url": "/ystavanpaiva/static/js/runtime~main.5b8865ae.js"
+  }
+]);
